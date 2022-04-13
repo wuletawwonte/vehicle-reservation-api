c9cc2031fd07728d0ca84a66341afa0790a4fde0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-13 14:07:38 +0300

    Connect to mongodb atlass database using mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb+srv://wuletaw:NwlKrS9Li7ovO1S4@cluster0.buyq6.mongodb.net/vehicles?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString).then(() => {
+    console.log('Successfully connected to mongo db');
+}).catch((err) => {
+    console.log("Error: " + err);
+    console.error(err);
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello there');
