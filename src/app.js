d9b0ca2833eed37f74b64fd88ddb07e8f79d6241
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-13 14:16:44 +0300

    Handle CORS errors

--- src/app.js
+++ src/app.js
@@ -15,6 +15,17 @@ mongoose.connect(connectionString).then(() => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// handle CORS error
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if(req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "*");
+      return res.status(200).json({});
+  }
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello there');
 })
