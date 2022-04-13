ce09a3d1366c2edcd274880c645b6214d6c322d8
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-13 13:39:33 +0300

    Add server that returns data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
-const app = require('./src/app');
+const app = require('./src/app.js');
 
-const PORT = process.env.PORT || 3060;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
